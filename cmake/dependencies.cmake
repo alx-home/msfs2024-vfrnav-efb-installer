22b4ceed854f0aa00e54788b7e6fe0c289d3a7c9
Alexandre GARCIN <perso@alex-home.fr>
2025-04-11 22:54:55 +0200

    Add dependencies version

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -3,23 +3,23 @@ include(FetchContent)
 FetchContent_Declare(
     webview
     GIT_REPOSITORY https://github.com/webview/webview
-    GIT_TAG 0.12.0)
+    GIT_TAG 1.0.0)
 FetchContent_MakeAvailable(webview)
 
 FetchContent_Declare(
     json
     GIT_REPOSITORY https://github.com/webview/webview
-    GIT_TAG 0.12.0)
+    GIT_TAG 1.0.0)
 FetchContent_MakeAvailable(json)
 
 FetchContent_Declare(
     promise
     GIT_REPOSITORY https://github.com/webview/webview
-    GIT_TAG 0.12.0)
+    GIT_TAG 1.0.0)
 FetchContent_MakeAvailable(promise)
 
 FetchContent_Declare(
     windows
     GIT_REPOSITORY https://github.com/webview/webview
-    GIT_TAG 0.12.0)
+    GIT_TAG 1.0.0)
 FetchContent_MakeAvailable(windows)
